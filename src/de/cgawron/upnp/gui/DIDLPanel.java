e34c5ab4d62db9cc0b0bc56cc262815ebb4cc616
Christian Gawron <christian.gawron@gmail.com>
2012-05-25 12:33:53 +0200

    new panel for DIDL

---
+++ src/de/cgawron/upnp/gui/DIDLPanel.java
@@ -0,0 +1,12 @@
+package de.cgawron.upnp.gui;
+
+import javax.swing.JPanel;
+
+import org.teleal.cling.support.model.DIDLObject;
+
+public class DIDLPanel extends JPanel
+{
+	private static final long serialVersionUID = 1L;
+
+	private DIDLObject didlObject;
+}
